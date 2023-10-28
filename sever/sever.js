712babcc86ad062c3d879902887b81c4306cf820
Tai-Ku <116546516+Tai-Ku@users.noreply.github.com>
2023-10-28 21:25:52 +0700

    first-commit sever

---
+++ sever/sever.js
@@ -0,0 +1,16 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 8888;
+
+app.use(express.json()); // để thèn express có thể hiểu được data được gửi lên từ phía client á là đọc theo kiểu json
+app.use(express.urlencoded({ extended: true })); // giúp đọc được kiểu mảng hay là object
+
+app.use("/", (req, res) => {
+  res.send("SEVER ON");
+});
+
+app.listen(port, () => {
+  console.log("listening on port " + port);
+});
